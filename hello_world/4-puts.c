33157f88c302ebc83bd86766dddb97f6c7a22988
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-17 11:09:46 +0100

    aa

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,7 +7,7 @@
 int main(void)
 {
   /* Display "Programming is like building a multilingual puzzle" */
-puts(Programming is like building a multilingual puzzle);
+puts("Programming is like building a multilingual puzzle");
 
     /* Return 0 to indicate successful execution */
 return (0);
