06faf73451d4e4535097d2b5b60da997216f0f7f
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-17 11:04:08 +0100

    Quatro

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return:Always 0
+ */
+int main(void)
+{
+  /* Display "Programming is like building a multilingual puzzle" */
+puts(Programming is like building a multilingual puzzle);
+
+    /* Return 0 to indicate successful execution */
+return (0);
+}
