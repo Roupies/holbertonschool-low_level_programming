6140680198ff2bbda3487ec226402e7341b056ac
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-25 10:10:52 +0100

    task 3 project C4

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_numbers - function to display number from 0 to 9
@@ -10,6 +11,6 @@ void print_numbers(void)
 {
 char ch = '0';
 while (ch <= '9')
-putchar (ch++);
-putchar('\n');
+_putchar (ch++);
+_putchar('\n');
 }
