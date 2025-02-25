d720f56b050c87cf09c9af339eea9bb69e597c12
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-25 13:35:48 +0100

    task 5 projet c4

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * more_numbers - function to display number from 0 to 14
- *
- * Return: 0 for successful
  */
 
 void more_numbers(void)
 {
-char ch;
-char chm;
+	int ch;
 
-for (ch = '0'; ch <= '9'; ch++)
-_putchar (ch);
-for (chm = '0'; chm <= '4'; chm++)
-putchar (chm);
+	for (ch = 0; ch < 15; ch++)
+	{
+		if (ch > 9)
+			_putchar((ch / 10) + '0');
+	_putchar((ch % 10) + '0');
+	}
+	_putchar('\n');
 }
