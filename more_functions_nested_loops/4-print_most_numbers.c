b2078840147034689836c1d4089fc492c45dfe61
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-25 10:39:06 +0100

    task 4 project C4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
- * print_numbers - function to display number from 0 to 9
+ * print_most_numbers - function to display number from 0 to 9 except 2 and 4
  *
  * Return: 0 for successful
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-char ch = '0';
-while (ch <= '9')
-_putchar (ch++);
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+if (ch != '2' && ch != '4')
+_putchar (ch);
 _putchar('\n');
 }
