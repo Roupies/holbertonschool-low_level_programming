84c0d22e8debfa27b4dc7f18b1506359376f9fdc
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-26 13:19:37 +0100

    task 9 project C4

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,29 +8,30 @@
 int main(void)
 {
 	int i;
-	for (i = 1; i <= 100; i++)
-		{
-			if (i % 3 == 0 && i % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else if (i % 5 == 0)
-			{
-				printf("Buzz");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz");
-			}
-			else
-			{
-				printf("%d", i);
-			}
 
-			if (i <= 100)
-			{
-				putchar(32);
-			}
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
 		}
-return (0);
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i <= 100)
+		{
+			putchar(32);
+		}
+	}
+	return (0);
 }
