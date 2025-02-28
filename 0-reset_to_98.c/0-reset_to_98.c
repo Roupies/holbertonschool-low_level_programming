f2170a035ef84f7d52c8ade891820db9ad134c45
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-28 09:21:22 +0100

    task 1 et 2 project c5

---
+++ 0-reset_to_98.c/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates the value to 98
+ * @n: is the integer we want to replace the value with 98
+ */
+
+void reset_to_98(int *n)
+{
+	int valeurdemandée = 98;
+	*n = valeurdemandée;
+		return;
+}
