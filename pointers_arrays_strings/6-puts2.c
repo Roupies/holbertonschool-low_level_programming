002a7abd561276f4a706cf2a8ab9a0472aa457ab
‘Roupies’ <maxime.naguet@gmail.com>
2025-02-28 17:01:04 +0100

    aa

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: is the string that we will print every other character
+ */
+
+void puts2(char *str)
+{
+	int printer = 0;
+
+	while (str[printer] != '\0')
+	{
+		_putchar(str[printer]);
+		printer++;
+		printer++;
+	}
+	_putchar('\n');
+}
