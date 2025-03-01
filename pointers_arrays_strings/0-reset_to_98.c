98d7080400e9664e13f33a591a2111f4b14b1ed1
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-01 10:59:22 +0100

    task6projectc5

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates the value to 98
+ * @n: is the integer we want to replace the value with 98
+ */
+
+void reset_to_98(int *n)
+{
+	int valeurdemandee = 98;
+	*n = valeurdemandee;
+}
