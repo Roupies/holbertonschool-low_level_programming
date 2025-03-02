e946ce37cc60ec1da62baf97c019833262e5fd66
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-02 21:15:27 +0100

    aa

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	/* Copy up to n bytes from src to dest */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	/* If less than n bytes were copied, fill the rest with null bytes */
 	for (; i < n; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
 
 
