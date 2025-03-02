ff770388100f4bd949767cb3cf6824b3956ed4c3
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-02 21:12:44 +0100

    aa

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,18 +6,15 @@
  * Return: A pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
+
 {
 	int i;
 
-	/* Copy up to n bytes from src to dest */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	/* If less than n bytes were copied, fill the rest with null bytes */
 	for (; i < n; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
-
-
