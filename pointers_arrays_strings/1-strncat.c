9219a469b85f67ddd1c416a51b7a11dd24c4a135
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-02 21:09:30 +0100

    aa

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,16 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = 0;
 	int i = 0;
 
-	/* Find the length of dest */
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	/* Append at most n bytes from src to dest */
 	while (i < n && src[i] != '\0')
 	{
 		dest[dest_len + i] = src[i];
 		i++;
 	}
 
-	/* Add terminating null byte if necessary */
 	if (i < n)
 		dest[dest_len + i] = '\0';
 
-	return dest;  // Add this line
-}
-
+	return (dest);
