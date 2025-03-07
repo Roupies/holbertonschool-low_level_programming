3a204d58bb69273192fabd89f40eb8738924d6aa
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-07 16:03:59 +0100

    aa

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Prints the name of the program
- * @argc: The number of command line arguments
+ * @argc: The number of command line arguments (unused in this program)
  * @argv: An array containing the program command line arguments
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argc; /* Suppress unused variable warning */
+	printf("%s\n", argv[0]); /* Print the program name (with path if included) */
 	return (0);
 }
 
