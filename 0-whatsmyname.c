eecc7da21f4db08394835408ecdeb50b3f38cf5c
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-07 16:12:19 +0100

    aa

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the program
- * @argc: The number of command line arguments (unused in this program)
+ * main - Prints the program name
+ * @argc: The number of command line arguments
  * @argv: An array containing the program command line arguments
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	(void)argc; 
-	printf("%s\n", argv[0]); 
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
