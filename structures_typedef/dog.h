2a4ca5e047834c8a0f5ccc899f49d0a1c74c3e7f
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-14 13:09:51 +0100

    taks 1 project structures/typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - attributs du chien
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#ENDIF
