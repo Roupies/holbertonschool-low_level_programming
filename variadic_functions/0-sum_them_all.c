049f1d31290e6b4187becd5006aa14dd1d7eb075
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-21 12:42:42 +0100

    task 0 project variadic functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - funcitpn that returns the sum of all its parameters
+ * @n: number of variadics arguments to come
+ * Return: the sum of all the integers given as variadic arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int somme = 0;
+	va_list params;
+	unsigned int i;
+
+	va_start(params, n);
+
+	for (i = 0; i < n; ++i)
+	{
+		somme += va_arg(params, unsigned int);
+	}
+	va_end(params);
+	return (somme);
+}
+
