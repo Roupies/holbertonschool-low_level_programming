0df298b1a0945f7c01cb46d814e1052411ce401e
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-24 08:08:33 +0100

    task 2 and 3 for project variadic functions

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - Prints strings separated by a given separator
+ * @separator: String to print between strings
+ * @n: Number of strings to print
+ * @...: Variable list of strings
+ *
+ * Description:
+ * - Prints each string from the variable arguments.
+ * - If a string is NULL, prints "(nil)" instead.
+ * - If the separator is NULL, it is not printed between strings.
+ * - Prints a new line at the end.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
+}
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - Prints values based on a format string
+ * @format: Format string specifying types of arguments
+ * @...: Variable arguments to print
+ *
+ * Description:
+ * - Prints values separated by ", ".
+ * - For strings, prints "(nil)" if NULL.
+ * - Ignores invalid format specifiers.
+ * - Ends with a new line.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *s;
+	char *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				sep = ", ";
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", sep, s);
+				sep = ", ";
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
