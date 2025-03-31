32c47bac92f7333a9ab0acb58ffc991675ba8072
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-31 13:43:06 +0200

    add the c file for the completion of the second task of the project singly linked lists

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len  - returns the number of elements in a linked list_t list.
+ * @h: is pointer to the first node of the list
+ *
+ * Return: the number of elements
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
