061e406a2b843b94254ef2a6bd64230898ab4684
‘Roupies’ <maxime.naguet@gmail.com>
2025-04-01 11:30:06 +0200

    modification of the c file for the fifth task of project singly linked lists, betty doc & betty style

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 #include <stdlib.h>
 
-/*
- * free_list - function that frees a list_t list
+/**
+ * free_list - frees a list_t list
  * @head: pointer to the first node of the list
  *
+ * Description: This function traverses the entire linked list, frees each
+ * node's string (if dynamically allocated), and then frees the node itself.
  */
-
 void free_list(list_t *head)
 {
-list_t *temp; 
+	list_t *temp;
 
-while (head != NULL)
-{
-	temp = head; 
-	head = head->next;
-	free(temp->str);
-	free(temp);
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
-}
+
